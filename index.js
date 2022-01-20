cbcde487e897ee3d33443912adc7cd3ebdcb548d
Dawson Vaught <vaught.dawson@gmail.com>
2022-01-20 12:58:47 -0700

    Disabled REST API for now.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { Intents } = require('discord.js');
 const botIntents = new Intents();
 botIntents.add(Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS)
 
-//const client = new DiscordBot(process.env.DISCORD_TOKEN, botIntents);
+const client = new DiscordBot(process.env.DISCORD_TOKEN, botIntents);
 
-const { ApiServer } = require('./api/ApiServer.js');
-const apiServer = new ApiServer('3000');
+// const { ApiServer } = require('./api/ApiServer.js');
+// const apiServer = new ApiServer('3000');
