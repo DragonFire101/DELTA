892964d7500687facda596c92dbb2f38b6158042
DragonFire101 <cjolly@zoominternet.net>
2023-06-05 06:22:42 -0400

    Added date and lowest rank to new members

--- functions/addMemberToSheet.js
+++ functions/addMemberToSheet.js
@@ -1,9 +1,13 @@
 async function addMemberToSheet(member, sheet, server) {
 	let row = {};
 
+	let today = new Date();
 	row[server.nameHeader] = member.name;
 	row[server.discordHeader] = member.id || 'None';
-
+	row[server.lastPromotionDateHeader] = today.getUTCMonth().toString().padStart(2,"0")+"/"+today.getUTCDate().toString().padStart(2,"0")+"/"+today.getUTCFullYear().toString().slice(2,4);
+	let structure = server.rankStructure;
+	const { ranks } = require(`../information/ranks/${structure}.json`);
+	row[server.rankHeader] = ranks[0].name;
 	await sheet.addRow(row);
 }
 
